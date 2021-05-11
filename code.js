2db7f1fde3d6630481c7c2d9e540a543cb468535
empulkowski <empulkowski@gmail.com>
2021-05-10 19:10:42 -0500

    Initial commit

---
+++ code.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    $("button").on("click", sortName);
+});
+
+
+
+// get user input for name
+
+//get user input for last name
+
+// get user input for points received
+
+// get user input for points possible
+
+//
